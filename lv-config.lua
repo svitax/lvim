9098c8ae90a900fca7be37ae9ef3e7d4f81710cc
Igor Barale <svitaxiom@gmail.com>
2021-07-28 18:25:15 -0400

    Disable lsp virtual text

--- lv-config.lua
+++ lv-config.lua
@@ -50,6 +50,7 @@ lvim.builtin.treesitter.highlight.enabled = true
 lvim.builtin.treesitter.indent.disable = { "python" }
 
 -- NOTE: Generic LSP settings
+lvim.lsp.diagnostics.virtual_text = false
 -- you can set a custom on_attach function that will be used for all the language servers
 -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
 -- lvim.lsp.on_attach_callback = function(client, bufnr)
