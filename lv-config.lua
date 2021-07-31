c1b3a014f3e9dab14db5b9ef179f43cf6c15647f
Igor Barale <svitaxiom@gmail.com>
2021-07-31 00:50:54 -0400

    add rnvimr and mappings

--- lv-config.lua
+++ lv-config.lua
@@ -252,6 +252,27 @@ lvim.plugins = {
 			-- vim.g.matchup_matchparen_enabled = 0
 		end,
 	},
+	{
+		"kevinhwang91/rnvimr",
+		cmd = "RnvimrToggle",
+		config = function()
+			-- " Make Ranger replace Netrw and be the file explorer
+			vim.g.rnvimr_enable_ex = 1
+			-- " Make Ranger to be hidden after picking a file
+			vim.g.rnvimr_enable_picker = 1
+			-- " Disable a border for floating window
+			-- vim.g.rnvimr_draw_border = 0
+			-- " Hide the files included in gitignore
+			-- vim.g.rnvimr_hide_gitignore = 1
+			-- " Make Neovim wipe the buffers corresponding to the files deleted by Ranger
+			vim.g.rnvimr_enable_bw = 1
+			-- " Add a shadow window, value is equal to 100 will disable shadow
+			vim.g.rnvimr_shadow_winblend = 70
+			-- " Draw border with both
+			-- vim.g.rnvimr_ranger_cmd = 'ranger --cmd="set draw_borders both"'
+			vim.g.rnvimr_ranger_cmd = "/Users/svitax/.miniconda/envs/ranger/bin/ranger"
+		end,
+	},
 	-----[[------------]]-----
 	---        Web         ---
 	-----]]------------[[-----
@@ -370,6 +391,8 @@ lvim.builtin.which_key.mappings["l"]["w"] = {
 	"<cmd>TroubleToggle lsp_workspace_diagnostics<cr>",
 	"List workspace diagnostics",
 }
+
+lvim.builtin.which_key.mappings["r"] = { "<cmd>RnvimrToggle<cr>", "Ranger" }
 
 lvim.builtin.which_key.mappings["s"]["n"] = {
 	"<cmd>lua require('telescope.builtin').find_files({cwd = '/Users/svitax/Library/Mobile Documents/iCloud~md~obsidian/Documents/svitax'})<CR>",
