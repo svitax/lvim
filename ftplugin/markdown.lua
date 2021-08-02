eba8900fa28059c8a771e2957e23be58c26fb5f0
Igor Barale <svitaxiom@gmail.com>
2021-08-02 03:07:24 -0400

    still haven't figured out markdown formatting with prettier

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,14 +1,34 @@
-lvim.lang.markdown.formatters = {
-  {
-    exe = "prettier",
-    args = {},
-  },
-}
--- TODO: install vale (copy from abzcoding/lvim)
--- lvim.lang.markdown.linters = {
+-- lvim.lang.markdown.formatters = {
 --   {
---     -- @usage can be write_good or vale or markdownlint
---     exe = "vale",
+--     exe = "prettier",
 --     args = {},
 --   },
 -- }
+-- lvim.lang.markdown.linters = {
+--   {
+--     -- @usage can be write_good or vale or markdownlint
+--     -- exe = "vale",
+--     exe = "",
+--     args = {},
+--   },
+-- }
+
+-- TODO: markdown formatting not working with prettier
+lvim.lang.markdown = {
+  formatters = {
+    {
+      exe = "prettier",
+      args = {},
+    },
+  },
+  -- TODO: install vale (copy from abzcoding/lvim)
+  linters = {
+    -- exe = "vale",
+    exe = "",
+    args = {},
+  },
+  lsp = {
+    provider = "",
+    setup = {},
+  },
+}
