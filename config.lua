3f658cfaeb4d199b41bbb392610e7a2c988c374a
Igor Barale <svitaxiom@gmail.com>
2021-08-04 22:13:45 -0400

    refactor dashboard config into separate file

--- config.lua
+++ config.lua
@@ -8,16 +8,7 @@ lvim.colorscheme = "fennec-gruvbox"
 -- Dashboard (builtin)
 -- =========================================
 lvim.builtin.dashboard.active = true
--- TODO: lunarvim's footer isn't exposed for user config, but here's how I want the footer to look
--- this needs to be in init.lua
--- vim.g.start_time = vim.fn.reltime()
--- local packages = #(vim.fn.globpath("~/.local/share/lunarvim/site/pack/packer/start", "*", 0, 1))
--- local loadtime = vim.fn.printf("%.3f", vim.fn.reltimefloat(vim.fn.reltime(vim.g.start_time)))
--- vim.g.dashboard_custom_footer = {
---   "LunarVim loaded in " .. loadtime .. " seconds with " .. packages .. " plugins   ",
---   -- 
---   -- "LunarVim loaded " .. packages .. " plugins   in " .. loadtime .. "  seconds ",
--- }
+require("user.dashboard").config()
 
 -- Toggleterm (builtin)
 -- =========================================
