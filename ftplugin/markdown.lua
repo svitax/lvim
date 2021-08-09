aa84c80618c498cce14471c6d1e3e27675e3fe27
Igor Barale <svitaxiom@gmail.com>
2021-08-09 01:14:17 -0400

    no longer using vim-markdown

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,16 +1,23 @@
+print "im in markdown"
+-- vim.cmd "set conceallevel=2"
 -- -- TODO: markdown formatting not working with prettier
--- lvim.lang.markdown.formatters = {
---   {
---     exe = "prettier",
---     args = {},
---   },
--- }
--- lvim.lang.markdown.linters = {
---   {
---     -- TODO: install vale (copy from abzcoding/lvim)
---     -- @usage can be write_good or vale or markdownlint
---     -- exe = "vale",
---     exe = "",
---     args = {},
---   },
--- }
+lvim.lang.markdown.formatters = {
+  {
+    exe = "prettier",
+    args = {},
+    -- args = { "--stdin", "--stdin-filepath", "$FILENAME", "--write" },
+  },
+}
+lvim.lang.markdown.linters = {
+  --   {
+  --     -- TODO: install vale (copy from abzcoding/lvim)
+  --     -- @usage can be write_good or vale or markdownlint
+  --     -- exe = "vale",
+  --     exe = "",
+  --     args = {},
+  --   },
+  {
+    exe = "markdownlint",
+    args = {},
+  },
+}
