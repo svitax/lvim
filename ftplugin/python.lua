b9cba008aa88464da4691b5954d9c2d5432d1795
Igor Barale <svitaxiom@gmail.com>
2021-08-24 16:30:14 -0400

    reminder to setup debugging for python

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -18,3 +18,8 @@ lvim.lang.python.linters = {
     exe = "pylint",
   },
 }
+
+-- if lvim.builtin.dap.active then
+--   local dap_install = require "dap-install"
+--   dap_install.config("python_dbg", {})
+-- end
