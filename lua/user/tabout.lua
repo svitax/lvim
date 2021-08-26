3f83f9a75518f7aca212a91f98c558a2c61ffee4
Igor Barale <svitaxiom@gmail.com>
2021-08-26 00:29:59 -0400

    fixed tabout (tab no longer cycles through compe completions, but does cycle through vsnip)

--- lua/user/tabout.lua
+++ lua/user/tabout.lua
@@ -1,18 +1,34 @@
---TODO: tabout not working
 local M = {}
+
+local utils = {
+
+  -- check_back_space = function()
+  --   local col = vim.fn.col "." - 1
+  --   if col == 0 or vim.fn.getline("."):sub(col, col):match "%s" then
+  --     return true
+  --   else
+  --     return false
+  --   end
+  -- end,
+
+  replace_keycodes = function(str)
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
+  end,
+}
 
 M.config = function()
   local status_ok, tabout = pcall(require, "tabout")
   if not status_ok then
     return
   end
+
   tabout.setup {
     tabkey = "", -- key to trigger tabout, set to an empty string to disable
     backwards_tabkey = "", -- key to trigger backwards tabout, set to an empty string to disable
     act_as_tab = true, -- shift content if tab out is not possible
     act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
     enable_backwards = true, -- well ...
-    completion = true, -- if the tabkey is used in a completion pum
+    completion = false, -- if the tabkey is used in a completion pum
     tabouts = {
       { open = "'", close = "'" },
       { open = '"', close = '"' },
@@ -20,65 +36,45 @@ M.config = function()
       { open = "(", close = ")" },
       { open = "[", close = "]" },
       { open = "{", close = "}" },
+      { open = "<", close = ">" },
+      { open = "_", close = "_" },
+      { open = "*", close = "*" },
+      { open = "%", close = "%" },
     },
-    ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
+    ignore_beginning = false, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
     exclude = {}, -- tabout will ignore these filetypes
   }
-  local function replace_keycodes(str)
-    return vim.api.nvim_replace_termcodes(str, true, true, true)
-  end
 
+  -- Use (s-)tab to:
+  --- jump to prev/next snippet's placeholder
+  --- tabout of pair
   function _G.tab_binding()
-    if vim.fn.pumvisible() ~= 0 then
-      return replace_keycodes "<C-n>"
-    elseif vim.fn["vsnip#available"](1) ~= 0 then
-      return replace_keycodes "<Plug>(vsnip-expand-or-jump)"
+    if vim.fn.call("vsnip#jumpable", { 1 }) == 1 then
+      return utils.replace_keycodes "<Plug>(vsnip-jump-next)"
+      -- elseif check_back_space() then
+      --   return replace_keycodes "<Tab>"
+      --   -- return replace_keycodes "<Plug>(Tabout)"
     else
-      return replace_keycodes "<Plug>(Tabout)"
+      -- return vim.fn["compe#complete"]() -- < use this if you want <tab> to always offer completion
+      -- return replace_keycodes "<Tab>"
+      -- return utils.replace_keycodes "<Plug>(Tabout)"
+      return utils.replace_keycodes "<Plug>(TaboutMulti)"
     end
   end
 
   function _G.s_tab_binding()
-    if vim.fn.pumvisible() ~= 0 then
-      return replace_keycodes "<C-p>"
-    elseif vim.fn["vsnip#jumpable"](-1) ~= 0 then
-      return replace_keycodes "<Plug>(vsnip-jump-prev)"
+    if vim.fn.call("vsnip#jumpable", { -1 }) == 1 then
+      return utils.replace_keycodes "<Plug>(vsnip-jump-prev)"
     else
-      return replace_keycodes "<Plug>(TaboutBack)"
+      -- return utils.replace_keycodes "<Plug>(TaboutBack)"
+      return utils.replace_keycodes "<Plug>(TaboutBackMulti)"
     end
   end
 
   vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_binding()", { expr = true })
+  vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_binding()", { expr = true })
   vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_binding()", { expr = true })
+  vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_binding()", { expr = true })
 end
 
 return M
-
--- {
--- TODO: vsnip tab jumping not really working?
--- "abecodes/tabout.nvim",
--- config = function()
---   require("tabout").setup {
---     tabkey = "<Tab>",
---     backwards_tabkey = "<S-Tab>",
---     act_as_tab = true,
---     act_as_shift_tab = false,
---     enable_backwards = true,
---     completion = true,
---     ignore_beginning = true,
---     tabouts = {
---       { open = "'", close = "'" },
---       { open = '"', close = '"' },
---       { open = "`", close = "`" },
---       { open = "(", close = ")" },
---       { open = "[", close = "]" },
---       { open = "{", close = "}" },
---       { open = "<", close = ">" },
---     },
---     exclude = {},
---   }
--- end,
--- requires = "nvim-treesitter/nvim-treesitter",
--- after = "nvim-compe",
--- event = "InsertEnter",
--- },
