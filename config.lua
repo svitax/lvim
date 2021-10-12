fc6fe19dc508d4a9858eccc1e1e95d8278068d09
Igor Barale <svitaxiom@gmail.com>
2021-10-12 12:36:05 -0400

    fix telescope-fzf-native

--- config.lua
+++ config.lua
@@ -203,6 +203,9 @@ lvim.plugins = {
   {
     "nvim-telescope/telescope-fzf-native.nvim",
     run = "make",
+    config = function()
+      require("telescope").load_extension "fzf"
+    end,
   },
   {
     "kazhala/close-buffers.nvim",
