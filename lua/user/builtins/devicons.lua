05cd8d89e813d5403640d2502e4fa90a316c18f5
Igor Barale <svitaxiom@gmail.com>
2021-10-12 01:24:56 -0400

    add custom devicons

--- lua/user/builtins/devicons.lua
+++ lua/user/builtins/devicons.lua
@@ -3,18 +3,46 @@ local M = {}
 M.config = function()
   -- custom folder icon
   require("nvim-web-devicons").set_icon {
-    -- css = {
-    --   icon = "",
-    --   -- color = "#667FE3",
-    --   color = "#D3869B",
-    --   name = "CSS",
-    -- },
+    yml = {
+      icon = "",
+      color = "#FF5252",
+      name = "Yaml1",
+    },
+    yaml = {
+      icon = "",
+      color = "#FF5252",
+      name = "Yaml2",
+    },
+    toml = {
+      icon = "",
+      color = "#FF5252",
+      name = "Toml",
+    },
+    zsh = {
+      -- icon = "",
+      icon = "",
+      -- icon = "",
+      color = "#FF7043",
+      name = "Zsh",
+    },
+    [".zshrc"] = {
+      -- icon = "",
+      icon = "",
+      -- icon = "",
+      color = "#FF7043",
+      name = "ZshConfig",
+    },
     sol = {
       icon = "",
       -- color = "#458588",
       -- color = "#A661AB",
-      color = "#3A87CB",
+      color = "#0288D1",
       name = "Solidity",
+    },
+    json = {
+      icon = "",
+      color = "#E1AD2D",
+      name = "Json",
     },
     js = {
       icon = "",
@@ -50,7 +78,9 @@ M.config = function()
     },
     css = {
       icon = "",
-      color = "#3276B8",
+      --   icon = "",
+      color = "#42A5F5",
+      --   color = "#D3869B",
       name = "Css",
     },
     html = {
@@ -58,6 +88,22 @@ M.config = function()
       color = "#E44D26",
       name = "Html",
     },
+    vue = {
+      icon = "﵂",
+      -- icon = "",
+      color = "#64B587",
+      name = "Vue",
+    },
+    svelte = {
+      icon = "",
+      color = "#EC6237",
+      name = "Svelte",
+    },
+    -- swift = {
+    --   icon = "",
+    --   color = "#FE5E2F",
+    --   name = "Swift",
+    -- },
     [".eslintrc.json"] = {
       icon = "",
       color = "#7986CB",
@@ -73,17 +119,17 @@ M.config = function()
       color = "#7986CB",
       name = "Eslint",
     },
-    [".stylintrc.json"] = {
+    [".stylelintrc.json"] = {
       icon = "",
       color = "#ebdbb2",
       name = "Stylelint",
     },
-    [".stylintrc.js"] = {
+    [".stylelintrc.js"] = {
       icon = "",
       color = "#ebdbb2",
       name = "Stylelint",
     },
-    [".stylintrc.yml"] = {
+    [".stylelintrc.yml"] = {
       icon = "",
       color = "#ebdbb2",
       name = "Stylelint",
@@ -104,15 +150,33 @@ M.config = function()
       name = "Prettier",
     },
     ["package.json"] = {
-      icon = "",
+      -- icon = "",
+      icon = "",
       color = "#7EAE44",
       name = "NodePackage",
     },
     ["package-lock.json"] = {
-      icon = "",
+      -- icon = "",
+      icon = "",
       color = "#7EAE44",
       name = "NodePackageLock",
     },
+    ["yarn.lock"] = {
+      icon = "",
+      color = "#42A5F5",
+      name = "YarnLock",
+    },
+    [".gitignore"] = {
+      icon = "",
+      color = "#E64A19",
+      name = "GitIgnore",
+    },
+    ["go.mod"] = {
+      icon = "",
+      color = "#DA4F7A",
+      name = "GoMod",
+    },
+
     -- TODO: maybe someday devicon will have support for regexes in filename
     -- ["*.test.js"] = {
     --   icon = "ﭧ",
