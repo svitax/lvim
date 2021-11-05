fbf1642bdcfbb5a5abdfa085264aae063262bfc6
Igor Barale <svitaxiom@gmail.com>
2021-11-05 14:46:14 -0400

    delete unused on_config

--- lua/user/builtins/nvimtree.lua
+++ lua/user/builtins/nvimtree.lua
@@ -30,13 +30,6 @@ M.config = function()
     },
   }
   lvim.builtin.nvimtree.git_hl = 0
-  -- lvim.builtin.nvimtree.on_config_done = function()
-  --   local tree_cb = require("nvim-tree.config").nvim_tree_callback
-  --   vim.g.nvim_tree_bindings = {
-  --     { key = { ";", "<CR>", "o" }, cb = tree_cb "edit" },
-  --     { key = "l", cb = tree_cb "close_node" },
-  --     { key = "v", cb = tree_cb "vsplit" },
-  --   }
 end
 
 return M
