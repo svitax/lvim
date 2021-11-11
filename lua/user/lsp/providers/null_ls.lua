f6c50018a365116f29c5130165fd6a776793099a
Igor Barale <svitaxiom@gmail.com>
2021-11-10 21:58:07 -0500

    fixed a null_ls bug

--- lua/user/lsp/providers/null_ls.lua
+++ lua/user/lsp/providers/null_ls.lua
@@ -74,8 +74,8 @@ M.config = function()
       },
       -- nls.builtins.formatting.rustywind,
       -- nls.builtins.formatting.scalafmt,
-      -- nls.builtins.formatting.shfmt.with { extra_args = { "-i", "2", "-ci" } },
-      -- nls.builtins.formatting.shellharden,
+      nls.builtins.formatting.shfmt.with { extra_args = { "-i", "2", "-ci" } },
+      nls.builtins.formatting.shellharden,
       nls.builtins.formatting.stylelint.with {
         filetypes = { "scss", "less", "css", "sass" },
       },
@@ -89,7 +89,7 @@ M.config = function()
         filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "svelte" },
       },
       -- nls.builtins.diagnostics.chktex,
-      -- nls.builtins.diagnostics.flake8,
+      nls.builtins.diagnostics.flake8,
       nls.builtins.diagnostics.hadolint,
       nls.builtins.diagnostics.luacheck,
       nls.builtins.diagnostics.markdownlint,
@@ -102,68 +102,6 @@ M.config = function()
       nls.builtins.diagnostics.yamllint,
     },
   }
-
-  -- WARN: do not redfine or reuse formatter/linters in this format
-  -- or use the lang specific format
-
-  -- Formatters
-  -- ==================================================================
-  lvim.lang.sh.formatters = {
-    { exe = "shfmt", args = { "-i", "2", "-ci" } },
-    { exe = "shellharden" },
-  }
-  lvim.lang.python.linters = { { exe = "flake8" } }
-  -- lvim.lang.markdown.linters = {
-  --   { exe = "markdownlint" },
-  --   -- { exe = "vale", },
-  -- }
-  -- lvim.lang.python.formatters = {
-  --   { exe = "black", args = { "--fast" } },
-  --   { exe = "isort", args = { "--profile", "black" } },
-  -- }
-  -- lvim.lang.sh.linters = { { exe = "shellcheck" } }
-  -- -- lvim.lang.asm.formatters = { { exe = "asmfmt" } }
-  -- -- lvim.lang.cmake.formatters = { { exe = "cmake_format" } }
-  -- lvim.lang.css.formatters = { { exe = "prettierd" } }
-  -- -- lvim.lang.dockerfile.linters = { { exe = "hadolint" } }
-  -- lvim.lang.go.formatters = {
-  --   -- @usage gofmt or gofumpt
-  --   { exe = "gofumpt" },
-  --   -- this already uses goimports by default
-  --   { exe = "golines" },
-  --   -- { exe = "goimports", },
-  -- }
-  -- FIX: html breaks prettierd on everything else
-  -- lvim.lang.html.formatters = { { exe = "prettier" } }
-  -- lvim.lang.javascript.formatters = { { exe = "prettierd" } }
-  -- lvim.lang.javascriptreact.formatters = { { exe = "prettierd" } }
-  -- lvim.lang.json.formatters = { { exe = "prettierd" } }
-  -- lvim.lang.less.formatters = { { exe = "prettierd" } }
-  -- lvim.lang.lua.formatters = { { exe = "stylua" } }
-  -- lvim.lang.markdown.formatters = { { exe = "prettierd" } }
-  -- -- -- lvim.lang.scala.formatters = { { exe = "scalafmt" } }
-  -- lvim.lang.solidity.formatters = { { exe = "prettierd" } }
-  -- -- lvim.lang.sql.formatters = { { exe = "sqlformat" } }
-  -- -- lvim.lang.terraform.formatters = { { exe = "terraform_fmt" } }
-  -- lvim.lang.typescript.formatters = { { exe = "prettierd" } }
-  -- lvim.lang.typescriptreact.formatters = { { exe = "prettierd" } }
-  -- -- lvim.lang.vue.formatters = { { exe = "prettierd" } }
-  -- lvim.lang.yaml.formatters = { { exe = "prettierd" } }
-  -- lvim.lang.nginx.formatters = { { exe = "nginx_beautifier" } }
-  -- lvim.lang.perl.formatters = { { exe = "perltidy" } }
-  -- lvim.lang.php.formatters = { { exe = "phpcbf" } }
-
-  -- Linters
-  -- ==================================================================
-  -- lvim.lang.css.linters = { { exe = "eslint_d" } }
-  -- lvim.lang.javascript.linters = { { exe = "eslint_d" } }
-  -- lvim.lang.javascriptreact.linters = { { exe = "eslint_d" } }
-  -- lvim.lang.json.linters = { { exe = "eslint_d" } }
-  -- lvim.lang.lua.linters = { { exe = "luacheck" } }
-  -- lvim.lang.typescript.linters = { { exe = "eslint_d" } }
-  -- lvim.lang.typescriptreact.linters = { { exe = "eslint_d" } }
-  -- lvim.lang.tex.linters = { { exe = "chktex" } }
-  -- lvim.lang.vim.linters = { { exe = "vint" } }
 end
 
 return M
