649ed685bb0d96273590f0a9fa40def0be418df9
Igor Barale <svitaxiom@gmail.com>
2021-11-10 21:36:13 -0500

    try winenter for vim-sandwich later

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -492,6 +492,7 @@ M.config = function()
       config = function()
         vim.cmd "runtime macros/sandwich/keymap/surround.vim"
       end,
+      -- event = "WinEnter"
     },
     { "chaoren/vim-wordmotion", event = "BufRead" },
     -- {
