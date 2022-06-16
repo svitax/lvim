ea857459126bbde0d0f837ebce8324abfad047f7
Igor Barale <svitaxiom@gmail.com>
2022-06-15 23:04:42 -0700

    some dapui things

--- lua/user/plugins/dap.lua
+++ lua/user/plugins/dap.lua
@@ -9,18 +9,6 @@ M.config = function()
 
   local icons = require "user.icons"
   vim.fn.sign_define("DapBreakpoint", { text = icons.ui.Bug, texthl = "DiagnosticSignError", linehl = "", numhl = "" })
-
-  local dap = require "dap"
-  local dapui = require "dapui"
-  dap.listeners.after.event_initialized["dapui_config"] = function()
-    dapui.open()
-  end
-  dap.listeners.before.event_terminated["dapui_config"] = function()
-    dapui.close()
-  end
-  dap.listeners.before.event_exited["dapui_config"] = function()
-    dapui.close()
-  end
 end
 
 return M
--- lua/user/plugins/dapui.lua
+++ lua/user/plugins/dapui.lua
@@ -48,6 +48,18 @@ M.config = function()
     },
     windows = { indent = 1 },
   }
+
+  local dap = require "dap"
+  local dapui = require "dapui"
+  dap.listeners.after.event_initialized["dapui_config"] = function()
+    dapui.open()
+  end
+  dap.listeners.before.event_terminated["dapui_config"] = function()
+    dapui.close()
+  end
+  dap.listeners.before.event_exited["dapui_config"] = function()
+    dapui.close()
+  end
 end
 
 return M
