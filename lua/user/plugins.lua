811b8245f3ee68030efe41726dd0c565b56e3086
Igor Barale <svitaxiom@gmail.com>
2022-10-27 22:28:36 -0700

    unnecessary comments

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -183,11 +183,7 @@ M.config = function()
         require("noice").setup()
       end,
       requires = {
-        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
         "MunifTanjim/nui.nvim",
-        -- OPTIONAL:
-        --   `nvim-notify` is only needed, if you want to use the notification view.
-        --   If not available, we use `mini` as the fallback
         "rcarriga/nvim-notify",
       },
     },
