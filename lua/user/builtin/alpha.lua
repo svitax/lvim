61d5d739bd9abcb0e0d73e24a64a6700025f7db0
Igor Barale <svitaxiom@gmail.com>
2022-11-07 20:49:02 -0800

    refactor(alpha): use smart_quit in alpha dashboard

--- lua/user/builtin/alpha.lua
+++ lua/user/builtin/alpha.lua
@@ -18,7 +18,7 @@ if status_ok then
       "<cmd>lua require('telescope').extensions.recent_files.pick(require('telescope.themes').get_ivy{})<cr>"
     ),
     button("c", lvim.icons.ui.Gear .. "  Configuration", "<cmd>edit " .. require("lvim.config"):get_user_config_path() .. " <cr>"),
-    button("q", lvim.icons.ui.BoldClose .. "  Quit", "<cmd>qa!<cr>"),
+    button("q", lvim.icons.ui.BoldClose .. "  Quit", "<cmd>lua require('user.usercmd').smart_quit()<cr>"),
     -- { "n", "  New File", "<CMD>ene!<CR>" },
     -- { "<spc> s f", "  Find File", "<CMD>lua require('user.telescope.custom_pickers').find_files()<CR>" },
     -- { "<spc> s p", "  Recent Projects ", "<CMD>lua require('user.telescope.custom_pickers').projects()<CR>" },
